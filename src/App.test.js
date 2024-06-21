106a7b224af649e43394ee5d081c393c75addd2f
devpedrofurquim <pedrofurquim.dev@gmail.com>
2024-06-21 17:22:08 -0300

    feat: Change tests name to match conventions of jest and rtl

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByTestId('repo-link');
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  it('should render repository button', () => {
+    render(<App />);
+    const linkElement = screen.getByTestId('repo-link');
+    expect(linkElement).toBeInTheDocument();
+  });
 
-test('url is correct', () => {
-  render(<App />);
-  const linkElement = screen.getByTestId('website-link');
-  expect(linkElement.href).toContain('devpedrofurquim.github.io')
-});
+  it('should render website button', () => {
+    render(<App />);
+    const linkElement = screen.getByTestId('website-link');
+    expect(linkElement.href).toContain('devpedrofurquim.github.io')
+  });
+})
