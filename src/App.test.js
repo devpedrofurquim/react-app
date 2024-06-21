ae318d6c81bf376f3bb4a244504761cdc9b99568
devpedrofurquim <pedrofurquim.dev@gmail.com>
2024-06-21 17:25:25 -0300

    feat: Add render App test

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,11 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
+  it('should render App', () => {
+    render(<App/>);
+    expect(screen.getByText('Hello World')).toBeInTheDocument();
+  })
+
   it('should render repository button', () => {
     render(<App />);
     const linkElement = screen.getByTestId('repo-link');
